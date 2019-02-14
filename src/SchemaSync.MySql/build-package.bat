60ee42763d698fd921f80c7361ce98b34b127ef2
cpsa3 <1090303279@qq.com>
2019-02-14 17:04:19 +0800

    发布新版本

--- src/SchemaSync.MySql/build-package.bat
+++ src/SchemaSync.MySql/build-package.bat
@@ -1,8 +1,8 @@
 @echo off
 cd %~dp0
-dotnet clean MySQLSchemaSync.csproj -c Release
+dotnet clean SchemaSync.MySql.csproj -c Release
 rd /s /Q bin\Release
-dotnet build MySQLSchemaSync.csproj -c Release
+dotnet build SchemaSync.MySql.csproj -c Release
 
 dotnet nuget push bin\Release\*.nupkg -s http://nuget.xiaobao100.cn/nuget/xiaobao
 pause
